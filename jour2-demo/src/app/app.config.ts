ee10c943b6d539ad10f6b5b1bb64e2296dccdec9
Malik H <malik.h@webdevpro.net>
2023-12-07 14:48:44 +0100

    boucle et conditions

--- jour2-demo/src/app/app.config.ts
+++ jour2-demo/src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { Premier} from "./premier.component"
 
 import { routes } from './app.routes';
 
--- jour2-demo/src/app/exo1.component.ts
+++ jour2-demo/src/app/exo1.component.ts
@@ -1,10 +1,40 @@
 import { Component} from "@angular/core"
+import { CommonModule } from '@angular/common';
 
 @Component({
+    imports : [CommonModule] ,
     standalone: true ,
-    template : "<p>je suis l'exo 1</p>",
+    template : `
+        <hr>
+        <h1>{{ titre }}</h1>
+        <p>je suis l'exo 1  </p>
+        <h2>afficher les éléments d'un tableau</h2>
+        <p> {{ jours[0] }} {{ jours[1] }}</p>
+        <h2>faire une boucle dans les composants angulars</h2>
+        <ul>
+            <li *ngFor="let jour of jours" >{{ jour }}</li>
+        </ul>
+        <h2>les conditions </h2>
+        <p *ngIf="isVisible"> je suis affiché à l'écran</p>
+    `,
     selector : "exo1"
 })
 export class Exo1 {
-
+    // propriété dans la class 
+    public titre : string = "je suis le titre du composant" ;
+    public jours : Array<string> = ["lundi" , "mardi" , "mercredi"]
+    public isVisible : boolean = false ;
 }
+
+// envoyer des informations de class vers la vue (le template)
+
+// vous devez ajouter dans votre composant 
+// le module suivante
+// import { CommonModule } from '@angular/common';
+// déclarer le module dans le Décorateur de votre imports : [CommonModule] ,
+
+// vous avez accès aux Directives suivantes :
+// <p *ngFor="let item if tableau">{item}</p>
+// <p *ngIf="condition">afficher ici </p>
+
+// créer 

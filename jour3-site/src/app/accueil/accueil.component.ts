16037b01b2cd3152a3c533f1fbeb5ac4ffaa82fe
Malik H <malik.h@webdevpro.net>
2023-12-08 11:37:59 +0100

    page d'accueil

--- jour3-site/src/app/accueil/accueil.component.ts
+++ jour3-site/src/app/accueil/accueil.component.ts
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './accueil.component.css'
 })
 export class AccueilComponent {
-
+   public url : string = "https://via.placeholder.com/800x600";
+   public titre : string = "Bienvenue sur le site" ; 
+   public content : string = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor vitae sunt hic, ad nulla magni voluptate nemo. Odit blanditiis officia quidem assumenda at iure, cum excepturi saepe! Delectus vel quaerat laborum autem ut, modi enim dolores sapiente molestias non officiis iste ad quas nobis esse quisquam, magnam, dolorem ipsa minus quasi odio adipisci. Possimus recusandae modi, in ab quia hic dolorum doloribus id vitae blanditiis, consequatur, eos aperiam aut? Sapiente beatae ipsa velit quos. Debitis quidem ratione odit. Non, pariatur ullam quis facilis debitis molestiae aut impedit at voluptatum quas cum perferendis? Doloribus impedit voluptas, necessitatibus incidunt officia dolore dolores. Perferendis illum modi repellat odit eveniet impedit ratione suscipit aliquid facere! Distinctio minima ipsum dolore hic, a modi tempora. Repellendus asperiores fugiat dolore consequatur magnam tempore quisquam aspernatur, doloremque amet fuga repellat quas quo! Provident aut accusantium enim placeat, fugit optio quis, ipsa reprehenderit tempore voluptas omnis. Modi nulla nihil earum, exercitationem commodi accusamus maxime quibusdam a, molestias molestiae expedita? Ullam quae dolorem corporis illo omnis, harum non quod voluptate ab. Quo aut accusantium harum, suscipit iure ut ipsam iste molestiae! Maiores repellat, itaque dolore distinctio ad doloremque quaerat aliquam sunt minus numquam laboriosam nam? Corporis facilis corrupti non magnam.";
 }
+
+
